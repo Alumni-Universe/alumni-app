d3562f2e9a52cc47109acdea99c4eff39766d551
kboberg98 <kristian.boberg@no.experis.com>
2023-03-23 13:52:27 +0100

    feat: created postitem and postlist

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -8,32 +8,31 @@ const PostList: FC = () => {
   const { posts } = useContext(PostContext) as PostContextType;
 
   const createPostList = () => {
-    return posts.map((p: IPost, key: number) => {
-      return (
-        <div key={key}>
-          <PostItem
-            postId={p.postId}
-            lastUpdated={p.lastUpdated}
-            sender={p.sender}
-            postTarget={p.postTarget}
-            postMessage={p.postMessage}
-            senderId={p.senderId}
-            replyParentId={p.replyParentId}
-            targetUser={p.targetUser}
-            targetGroup={p.targetGroup}
-            targetTopic={p.targetTopic}
-            targetEvent={p.targetEvent}
-          />
-        </div>
-      );
-    });
+    return posts
+      .filter((p: IPost) => p.replyParentId === null)
+      .map((p: IPost, key: number) => {
+        return (
+          <div key={key}>
+            <PostItem
+              postId={p.postId}
+              lastUpdated={p.lastUpdated}
+              sender={p.sender}
+              postTarget={p.postTarget}
+              postMessage={p.postMessage}
+              senderId={p.senderId}
+              replyParentId={p.replyParentId}
+              targetUser={p.targetUser}
+              targetGroup={p.targetGroup}
+              targetTopic={p.targetTopic}
+              targetEvent={p.targetEvent}
+            />
+          </div>
+        );
+      });
   };
 
   return (
     <section>
-      <section style={{ textAlign: "center" }}>
-        <h4>Posts</h4>
-      </section>
       <hr className="mb-1" />
       <div>{createPostList()}</div>
     </section>
