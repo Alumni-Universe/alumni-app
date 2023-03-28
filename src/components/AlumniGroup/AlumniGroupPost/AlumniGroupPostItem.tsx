bc355ef950b77b1848ed9a13e56b68718734f426
Pavel <bavelibrahim@gmail.com>
2023-03-28 14:23:40 +0200

    Chore: getting ready for merge

---
+++ src/components/AlumniGroup/AlumniGroupPost/AlumniGroupPostItem.tsx
@@ -0,0 +1,17 @@
+
+import { FC } from "react";
+import { useContext } from "react";
+
+
+
+const AlumniGroupPostItem : FC = () => {
+
+    return (
+        <div>
+
+        </div>
+    );
+
+}
+
+export default AlumniGroupPostItem;
